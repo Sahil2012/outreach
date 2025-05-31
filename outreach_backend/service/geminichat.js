ad60de9ff6ed927d57b981a09707e0a6217d4096
unknown <sahilhsgupta7@gmail.com>
2025-05-31 14:26:14 +0530

    fix: chnaged the underlying model to 2.5-flash

--- outreach_backend/service/geminichat.js
+++ outreach_backend/service/geminichat.js
@@ -3,7 +3,7 @@ import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 async function callLLM(prompt) {
 
     const model = new ChatGoogleGenerativeAI({
-        model : 'gemini-2.0-flash',
+        model : 'gemini-2.5-flash-preview-05-20',
         apiKey : process.env.GEMINI_API_KEY,
         temperature : 0.9,
     });
