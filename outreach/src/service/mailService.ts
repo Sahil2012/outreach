567c6b8e103febd56b3e043c369239b6cecb77d2
unknown <sahilhsgupta7@gmail.com>
2025-06-08 15:45:58 +0530

    fix: removed type from body

--- outreach/src/service/mailService.ts
+++ outreach/src/service/mailService.ts
@@ -1,12 +1,11 @@
 import mailClient from '../apis/mailClient';
-import { SendEmail } from '../types';
 
 export const generateMail = async (data : object) => {
   const response = await mailClient.post('/generateMail', data);
   return response.data;
 };
 
-export const sendMail = async (data : SendEmail) => {
+export const sendMail = async (data : object) => {
   const response = await mailClient.post('/sendEmail', data);
   return response.data;
 };
