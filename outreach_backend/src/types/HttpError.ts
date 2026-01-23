a4983c9d0cc4626ec1397b9d9a145cb7636e57b5
Sahil Gupta <sahilhsgupta7@gmail.com>
2026-01-24 02:04:05 +0530

    chore: segregated errors object

--- outreach_backend/src/types/HttpError.ts
+++ outreach_backend/src/types/HttpError.ts
@@ -1,6 +1,24 @@
 
-interface HttpError extends Error {
-    status?: number;
+export class HttpError extends Error {
+    status: number;
+
+    constructor(status: number, message: string) {
+        super(message);
+        this.status = status;
+        this.name = this.constructor.name;
+    }
+}
+
+export class NotFoundError extends HttpError {
+    constructor(message: string) {
+        super(404, message);
+    }
+}
+
+export class BadRequestError extends HttpError {
+    constructor(message: string) {
+        super(400, message);
+    }
 }
 
 export default HttpError;
